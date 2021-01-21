868d7b3455c95bd1845bfcf24f203486865668e8
heymatch <match.remo@gmail.com>
2021-01-21 10:39:17 +0800

    Alert or Recommand Message Color

--- src/js/Chart.js
+++ src/js/Chart.js
@@ -42,23 +42,31 @@ async function displayChart(select){
             
             dataTable.addColumn("date", "Date");
             dataTable.addColumn("number", "Cumulative Confirmed");
-            dataTable.addColumn("number", "Predition Cumulative Confirmed");
             
             var request = await BigQuery.request("SELECT date, (cumulative_confirmed - cumulative_recovered) FROM bigquery-public-data.covid19_open_data.covid19_open_data WHERE LENGTH(location_key) = 2 and country_name = '" + country + "' and cumulative_confirmed != 0");
             //var simr = await SIR_simulation("constant", country);
             var simr = await BigQuery.request("SELECT date, pos FROM `bigdata-301014.beta_gamma.result` WHERE country_name = '" + country + "'");
             
             var arr = [];
-            var r = 0;
-            for(var i in request.result.rows){
-                ++r;
-                arr.push([new Date(request.result.rows[i].f[0].v), parseInt(request.result.rows[i].f[1].v), parseInt(simr.result.rows[i].f[1].v)]);
+            if(simr.result.totalRows != "0"){
+                dataTable.addColumn("number", "Predition Cumulative Confirmed");
+                var r = 0;
+                for(var i in request.result.rows){
+                    ++r;
+                    arr.push([new Date(request.result.rows[i].f[0].v), parseInt(request.result.rows[i].f[1].v), parseInt(simr.result.rows[i].f[1].v)]);
+                }
+                var today = new Date('2021-1-20');
+                while(r != 400){
+                    today.setDate(today.getDate() + 1);
+                    arr.push([new Date(today), 0, parseInt(simr.result.rows[r].f[1].v)]);
+                    ++r;
+                }
             }
-            var today = new Date('2021-1-20');
-            while(r != 400){
-                today.setDate(today.getDate() + 1);
-                arr.push([new Date(today), 0, parseInt(simr.result.rows[r].f[1].v)]);
-                ++r;
+            else{
+                addAlert("沒有預測的資料！")
+                for(var i in request.result.rows){
+                    arr.push([new Date(request.result.rows[i].f[0].v), parseInt(request.result.rows[i].f[1].v)]);
+                }
             }
             
             dataTable.addRows(arr);
@@ -142,23 +150,29 @@ async function displayChart(select){
             
             dataTable.addColumn("string", "Country");
             dataTable.addColumn("number", "Cumulative Confirmed");
+            dataTable.addColumn({type: "string", role: "style"});
             
             var request = await BigQuery.request("SELECT country_name, cumulative_confirmed FROM bigquery-public-data.covid19_open_data.covid19_open_data WHERE LENGTH(location_key) = 2 AND date = DATE_ADD(CURRENT_DATE(), INTERVAL -3 DAY) ORDER BY cumulative_confirmed desc LIMIT 30");
             var arr = [];
             var danger = false;
             for(var i in request.result.rows){
-                arr.push([request.result.rows[i].f[0].v, parseInt(request.result.rows[i].f[1].v)]);
                 if(request.result.rows[i].f[0].v == country){
+                    arr.push([request.result.rows[i].f[0].v, parseInt(request.result.rows[i].f[1].v), "red"]);
                     danger = true;
+                }
+                else{
+                    arr.push([request.result.rows[i].f[0].v, parseInt(request.result.rows[i].f[1].v), "blue"]);
                 }
             }
             dataTable.addRows(arr);
             
             if(danger){
-                addAlert("你要去的區域的累積確診人數在確診人數在30名以內!很危險")
+                addAlert("你要去的區域的累積確診人數在確診人數在30名以內！")
+                addAlert("<strong class='danger'>很危險<strong>")
             }
             else{
-                addAlert("你要去的區域的累積確診人數在確診人數在30名以外!比較安全")
+                addAlert("你要去的區域的累積確診人數在確診人數在30名以外！")
+                addAlert("<strong class='safe'>比較安全<strong>")
             }
 
             var options = {
